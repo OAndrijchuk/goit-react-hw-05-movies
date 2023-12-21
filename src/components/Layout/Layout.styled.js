ccbfa4f294be9c3e2fcb07a7ee24faf4a14918d8
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-12-21 21:34:46 +0200

    add style

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -34,3 +34,28 @@ export const NavLinkStyled = styled(NavLink)`
     transition: all 0.2s ease;
   }
 `;
+export const NavLinkStyled2 = styled(NavLink)`
+  color: black;
+  position: relative;
+  text-decoration: none;
+  transition: all 0.2s ease;
+  &.active {
+    color: gold;
+  }
+  &:hover:not(.active) {
+    transform: scale(1);
+    &::after {
+      transform: scale(1);
+    }
+  }
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 3px;
+    transform: scale(0);
+    background-color: currentColor;
+    transition: all 0.2s ease;
+  }
+`;
